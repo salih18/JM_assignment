9ec21d0723d36334437ab51c274bd5ffa1200e30
salih <salihsert18@gmail.com>
2020-05-07 13:17:51 +0200

    add search title to search results

--- client/src/components/movies/MoviesGrid.js
+++ client/src/components/movies/MoviesGrid.js
@@ -6,10 +6,13 @@ import { Col, CardImg, CardGroup, Container } from "reactstrap";
 
 import { IMAGE_BASE_URL } from "./../../utils/config";
 
-const MoviesGrid = ({ movies }) => {
+const MoviesGrid = ({ movies, search }) => {
   return (
     <Container>
-      <h1 className="text-center display-4 mb-5">Popular Movies</h1>
+      <h1 className="text-center display-4 mb-5">
+        {search.searchTerm ? "Search Result" : "Popular Movies"}
+      </h1>
+
       <CardGroup>
         {movies.map((movie) => (
           <Col md="3" key={movie.id} className="mb-4">
@@ -30,10 +33,12 @@ const MoviesGrid = ({ movies }) => {
 
 MoviesGrid.propTypes = {
   movies: PropTypes.array.isRequired,
+  search: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   movies: state.movies.data,
+  search: state.search,
 });
 
 export default connect(mapStateToProps, {})(MoviesGrid);
